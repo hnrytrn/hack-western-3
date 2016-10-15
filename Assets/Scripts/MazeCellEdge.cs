f67281a8af4278e13656545c3101f56ef9eefcc4
Henry Tran <22henrytran@gmail.com>
2016-10-15 01:05:47 -0400

    add random maze generation

---
+++ Assets/Scripts/MazeCellEdge.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class MazeCellEdge : MonoBehaviour {
+
+	public MazeCell cell, otherCell;
+
+	public MazeDirection direction;
+
+	public void Initialize (MazeCell cell, MazeCell otherCell, MazeDirection direction) {
+		this.cell = cell;
+		this.otherCell = otherCell;
+		this.direction = direction;
+		cell.SetEdge(direction, this);
+		transform.parent = cell.transform;
+		transform.localPosition = Vector3.zero;
+		transform.localRotation = direction.ToRotation();
+	}
+}
